12b051a39642c3081aabee7dcbd7a33ca8116e80
leobessa <leobessa@gmail.com>
2024-05-28 11:33:32 -0300

    chore: created new Elixir project

---
+++ lib/whiskey_sour.ex
@@ -0,0 +1,18 @@
+defmodule WhiskeySour do
+  @moduledoc """
+  Documentation for `WhiskeySour`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WhiskeySour.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/whiskey_sour_test.exs
@@ -0,0 +1,8 @@
+defmodule WhiskeySourTest do
+  use ExUnit.Case
+  doctest WhiskeySour
+
+  test "greets the world" do
+    assert WhiskeySour.hello() == :world
+  end
+end
