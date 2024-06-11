8bd9b8df474d3297f81d1edc6cbfcbadfd38f010
leobessa <leobessa@gmail.com>
2024-06-10 22:18:01 -0300

    feat: Add job-related functions to ProcessInstance module

--- test/whiskey_sour/core/process_instance_test.exs
+++ test/whiskey_sour/core/process_instance_test.exs
@@ -20,9 +20,9 @@ defmodule WhiskeySour.Core.ProcessInstanceTest do
   end
 
   describe "start/1" do
-    test "starts the process instance" do
-      definition = sample_order_process_definition()
-      process_instance = ProcessInstance.construct(definition)
+    test "starts the sample_order_process_definition instance" do
+      process_instance =
+        ProcessInstance.construct(sample_order_process_definition())
 
       process_instance = ProcessInstance.start(process_instance)
 
@@ -39,6 +39,49 @@ defmodule WhiskeySour.Core.ProcessInstanceTest do
                  id: "token:1",
                  node_id: "review_order",
                  status: :active
+               }
+             ]
+    end
+  end
+
+  describe "jobs/1" do
+    test "returns the jobs for the process instance" do
+      process_instance =
+        sample_order_process_definition()
+        |> ProcessInstance.construct()
+        |> ProcessInstance.start()
+
+      jobs = ProcessInstance.jobs(process_instance)
+
+      assert jobs == [
+               %{
+                 node_id: "review_order",
+                 assignee: "user1",
+                 token_id: "token:1"
+               }
+             ]
+    end
+  end
+
+  describe "complete_job/2" do
+    test "completes the job for the given token" do
+      definition = sample_order_process_definition()
+      process_instance = ProcessInstance.construct(definition)
+      process_instance = ProcessInstance.start(process_instance)
+
+      process_instance = ProcessInstance.complete_job(process_instance, "token:1")
+
+      assert process_instance.uncommitted_events == [
+               %{id: "evt:start_event:1", node_id: "start_event", status: :completed},
+               %{id: "evt:review_order:1", node_id: "review_order", status: :completed},
+               %{id: "evt:end_event:1", node_id: "end_event", status: :completed}
+             ]
+
+      assert process_instance.tokens == [
+               %{
+                 id: "token:1",
+                 node_id: "end_event",
+                 status: :terminated
                }
              ]
     end
