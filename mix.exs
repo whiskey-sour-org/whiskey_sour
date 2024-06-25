6cccf2f6b0c640b75135a6ed026e238d452b749d
leobessa <leobessa@gmail.com>
2024-06-25 16:40:38 -0300

    chore: Update mix.exs to include preferred_cli_env for credo and sobelow

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule WhiskeySour.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: [
+        credo: :test,
+        sobelow: :test
+      ]
     ]
   end
 
@@ -21,9 +25,10 @@ defmodule WhiskeySour.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 1.7", only: [:test], runtime: false},
+      {:mix_audit, ">= 0.0.0", only: [:dev, :test], runtime: false},
+      {:sobelow, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:styler, "~> 1.0.0-rc.0", only: [:dev, :test], runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
